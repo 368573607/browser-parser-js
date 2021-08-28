6d1844be82b7bd31886bb29bd22d270c8b09f3ce
xuhuading(mac) <xuhuading@foxmail.com>
2021-08-28 19:27:26 +0800

    to: 修复webpack的错误

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
+const path = require('path');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     entry: {
-        'browser-parser': './src/index.js',
-        'browser-parser.min': './src/index.js',
+        'browser-parser': path.resolve(__dirname, 'src', 'index.js'),
+        'browser-parser.min': path.resolve(__dirname, 'src', 'index.js'),
     },
     output: {
         path: './dist',
